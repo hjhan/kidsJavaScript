06b3aeea234b7f75792050ce25b6e7cf6ad778b9
steve.han <steve.han@sumscope.com>
2016-09-12 14:02:57 +0800

    add NaN, Infinity

--- src/lesson2/lesson2.js
+++ src/lesson2/lesson2.js
@@ -1,1 +1,20 @@
-typeof 2323;
+typeof 2323;   //'number'
+typeof "String in quotes";   //'string'
+/**
+ * window(browser) or global (node)  NaN, Infinity
+ */
+
+typeof NaN;    //'number'
+
+NaN !== NaN;      //true
+
+Number.isNaN("dfjk");   //false  only NaN true
+
+Number("xyz");  //NaN;
+
+3/0;  //Infinity
+
+Infinity - Infinity;   //NaN
+Infinity + Infinity;   //Infinity
+Infinity / Infinity;   //NaN
+Infinity * Infinity;     //Infinity
