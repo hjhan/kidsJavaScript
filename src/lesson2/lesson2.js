7a97e8d900b209cfe98c278327fbaf1b2d77606d
Hongjun(Steve) Han <hongjunh@gmail.com>
2016-09-24 16:02:02 +0800

    for, while and if

--- src/lesson2/lesson2.js
+++ src/lesson2/lesson2.js
@@ -1,3 +1,30 @@
+/**
+ * if else
+ */
+let age = 10;
+if (age >= 18) {
+    console.log("成年人");
+} else {
+    console.log("青少年，幼儿");
+}
+
+/**
+ * while
+ */
+
+var i = 0;
+
+while (i < 100) {
+    console.log('i当前为：' + i);
+    i += 1;
+}
+
+/**
+ * for loop 判断偶数 或 质数/素数
+ */
+
+
+
 typeof 2323;   //'number'
 typeof "String in quotes";   //'string'
 /**
