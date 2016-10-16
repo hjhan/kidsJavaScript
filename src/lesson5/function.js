bd997c7ad3802523582f9b7e67a3e15d00ddd624
Hongjun(Steve) Han <hongjunh@gmail.com>
2016-10-16 22:14:19 +0800

    Decomposition and Abstraction for functions

--- src/lesson5/function.js
+++ src/lesson5/function.js
@@ -1,10 +1,15 @@
 /**
- * why function, function name, parameter, default parameter, variable parameter, return value.
+ * why function（Decomposition分解与抽象 spec), function name, parameter, default parameter, variable parameter, return value.
  */
 
 
 /**
  * nested function  and high order function
+ */
+
+
+/**
+ * recursion fibonacci number
  */
 
 
