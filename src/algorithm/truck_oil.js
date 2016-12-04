48e56b58cbfff421b682053cd911f1e118cb8fbd
Hongjun(Steve) Han <hongjunh@gmail.com>
2016-12-04 08:39:59 +0800

    optimal truck oil problem:extract function

--- src/algorithm/truck_oil.js
+++ src/algorithm/truck_oil.js
@@ -6,13 +6,22 @@
  min(14*x + 9*y)
  x >=0 && y >=0
  */
-var optimal = {};
-for (var x = 0; x <= 8; x++) {
-    var y = Math.ceil((59 - 7 * x) / 4);
-    var oil = 14 * x + 9 * y;
-    console.log("x:%d,y:%d,oil:%d,optimal.min:%d", x, y, oil, optimal.min);
-    if (optimal.min === undefined || optimal.min > oil) {
-        optimal = {x: x, y: y, min: oil};
+function optimalOil(total, big, small) {
+    var optimal = {};
+    var xMax = Math.ceil(total / big.load);
+    for (var x = 0; x <= xMax; x++) {
+        var y = Math.ceil((total - big.load * x) / small.load);
+        if (y < 0) continue;
+        var oil = big.oilCon * x + small.oilCon * y;
+        console.log("x:%d,y:%d,oil:%d,optimal.min:%d", x, y, oil, optimal.min);
+        if (optimal.min === undefined || optimal.min > oil) {
+            optimal = {x: x, y: y, min: oil};
+        }
     }
+    console.log("最少耗油：%d，大货车:%d趟，小货车:%d趟", optimal.min, optimal.x, optimal.y);
+    return optimal;
 }
-console.log("最少耗油：%d，大货车:%d趟，小货车:%d趟", optimal.min, optimal.x, optimal.y);
+var total = 59;
+var big = {load: 7, oilCon: 14};
+var small = {load: 4, oilCon: 9};
+optimalOil(total, big, small);
